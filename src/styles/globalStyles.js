efe5aad1f517368f71a9ae2b4ca30d52c388d947
jeffersonxbenetti <jefferson.benetti@hotmail.com>
2024-08-07 21:58:16 -0300

    Instalando styled Components e colocando os estilos globais.

---
+++ src/styles/globalStyles.js
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        outline: none;
+        border: none;
+        list-style: none;
+        text-decoration: none;
+        font-family: "Montserrat", sans-serif;
+    }
+
+    body {
+        background: #000000;
+    }
+`
